05017bccc0866e31c8026fd4e36b01e134aedca3
Noahbrrt <noah.barret@edu.univ-fcomte.fr>
2025-06-05 14:53:45 +0200

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['SF Pro Display', 'sans-serif'],
+      },
     },
-    plugins: [],
-  };
-
-  
+  },
+  plugins: [],
+};  
